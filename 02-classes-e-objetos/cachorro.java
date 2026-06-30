c95106c99a07dd87766816bae12f7a840d614049
Lorena Carvalho <lorena.santos0506@edu.unifor.br>
2026-06-30 17:11:49 -0300

    Create cachorro.java

---
+++ 02-classes-e-objetos/cachorro.java
@@ -0,0 +1,25 @@
+public class Cachorro {
+
+    // Atributos (Características)
+    public String nome;
+    public int peso;
+    public String corOlhos;
+    public int numPatas;
+
+    // Métodos (Comportamentos)
+    public void falar() {
+        System.out.println(nome + " diz: Au Au!");
+    }
+
+    public void andar() {
+        System.out.println(nome + " está andando...");
+    }
+
+    public void comer() {
+        System.out.println(nome + " está comendo ração.");
+    }
+
+    public void dormir() {
+        System.out.println(nome + " dormiu... Zzz");
+    }
+}
